e4a99e032bf6d001dbb64e658a4055c80c709501
gabrielebottari <gabriele.bottari1@gmail.com>
2024-01-19 14:38:22 +0100

    ADD single card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,96 @@
 <script>
+
+import SingleCard from './SingleCard.vue'
+
 export default {
     data() {
         return {
+            comics: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                },
+            ]
 
         };
     },
+    
+    props : {
+    thumb : String,
+    series : String
+    },
+    components: {
+        SingleCard
+    },
+
     methods: {
 
     }
@@ -13,23 +99,68 @@ export default {
 
 <template>
     <main>
+        <div class="jumbotron">
+            <img src="../../public/img/jumbotron.jpg" alt="jumbotron">
+        </div>
+       
+
        <div class="container">
-       Content Goes Here
+
+        <div class="cards-container">
+            <SingleCard
+                v-for="(comic, index) in comics"
+                :key = "index"
+                :thumb = "comic.thumb"
+                :series = "comic.series"
+            />  
+        </div>
+
        </div>
+
     </main>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+@use "../assets/scss/partials/variables.scss" as *;
 
 main {
-    background-color: black;
-    .container{
-        height: 315px;
-        color: white;
-        font-size: 3em;
-        text-align: center;
-        line-height: 300px;
+    background-color: #1c1c1c;
+
+    .jumbotron {
+      max-height: 400px;
+      overflow:  hidden;
+
+      img {
+        width: 100%;
+      }
+
     }
-}
+
+    .container {
+      position: relative;
+      color : white;
+      padding : 60px 0 20px;
+
+      h1 {
+        position: absolute;
+        top: -30px;
+        left: 0;
+        display: inline-block;
+        text-transform: uppercase;
+        color : white;
+        background-color : $primary-color;
+        padding: 10px 30px;
+      }
+
+      .cards-container {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+    }
+
+  }
+
 
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+    props: {
+        thumb: String,
+        series: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="thumb">
+            <img :src="thumb" :alt="series">
+        </div>
+        <h5>{{ series }}</h5>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6);
+    padding: 0 10px;
+
+    .thumb {
+      margin-bottom: 20px;
+      overflow: hidden;
+      height: 220px;
+      position: relative;
+         
+      img {
+        width: 100%;
+        position: absolute;
+        margin: auto;
+        top: 0px;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        }
+    }
+
+    h5 {
+      text-transform: uppercase;
+      margin-bottom: 50px;
+    }
+}
+</style>
